816560fc7125fbc5d3738b8d6df93190cf26e4b0
xxaviere <75096855+xxaviere@users.noreply.github.com>
2021-01-20 21:53:18 +0700

    Add files via upload

---
+++ database/migrations/2021_01_19_223432_create_members_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nim');
+            $table->string('konsentrasi');
+            $table->string('judul');
+            $table->string('proposal');
+            $table->string('pembimbing');
+            $table->string('status');
+            $table->string('action');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
---
+++ resources/views/livewire/members.blade.php
@@ -0,0 +1,62 @@
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Data Proposal
+    </h2>
+</x-slot>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+            @if (session()->has('message'))
+                <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3" role="alert">
+                    <div class="flex">
+                        <div>
+                            <p class="text-sm">{{ session('message') }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+         
+            
+            @if($isModal)
+                @include('livewire.create')
+            @endif
+
+            <table class="table-fixed w-full">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="px-4 py-2">Nama</th>
+                        <th class="px-4 py-2">NIM</th>
+                        <th class="px-4 py-2">Konsentrasi</th>
+                        <th class="px-4 py-2 w-20">Judul</th>
+                        <th class="px-4 py-2">Proposal</th>
+                        <th class="px-4 py-2">Pembimbing</th>
+                        <th class="px-4 py-2">Status</th>
+                        <th class="px-4 py-2">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($members as $row)
+                        <tr>
+                            <td class="border px-4 py-2">{{ $row->nama }}</td>
+                            <td class="border px-4 py-2">{{ $row->nim }}</td>
+                            <td class="border px-4 py-2">{{ $row->konsentrasi }}</td>
+                            <td class="border px-4 py-2">{{ $row->judul }}</td>
+                            <td class="border px-4 py-2">{{ $row->proposal }}</td>
+                            <td class="border px-4 py-2">{{ $row->pembimbing }}</td>
+                            <td class="border px-4 py-2">{{ $row->status }}</td>
+                            <td class="border px-4 py-2">
+                                <button wire:click="delete({{ $row->id }})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Decline</button>
+                                <button wire:click="create()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Approve</button>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td class="border px-4 py-2 text-center" colspan="5">Tidak ada data</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
